5ebf7df421622c37f9e068906e37159173763995
Md Shahazad Uddin <shanudjn@gmail.com>
2020-12-12 20:45:19 +0530

    feature : added function to call API and console logging

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const searchButton = document.querySelector('.div-search__button');
 const inputElement = document.querySelector('.div-search__input');
+const characterListDiv = document.querySelector('#character-list');
+
+const url = "https://breakingbadapi.com/api/characters";
 
 
 
 
+
+
+function displayData(data) {
+    console.log(data[0].name);
+    characterListDiv.innerText = (data[0].name);
+}
 
 function getSearchString() {
     console.log(inputElement.value);
+    console.log(data);
 }
 
 
 searchButton.addEventListener('click', getSearchString);
 
 (function () {
-    console.log("Firt thing that happens");
+    console.log("First thing that happens");
+    var response = fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            displayData(json);
+        })
 
 
 })();
